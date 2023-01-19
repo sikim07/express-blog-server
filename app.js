8071af2b5017c54face3031148bbcb497d621b94
sikim07 <skim88.1942@gmail.com>
2023-01-19 09:32:08 +0900

    test

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 5500
 
 app.get('/', (req, res) => {
+    console.log('[/] URL Called')
     res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+    console.log('[/test] URL Called')
+    res.json({
+        ok: 'true',
+        data: 'test'
+    })
 })
 
 app.listen(port, () => {
